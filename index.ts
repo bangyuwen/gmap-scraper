0c1d2edafd41f1d7325368a2dee75df1c7463bf3
BangYuWen <bangyuwen@gmail.com>
2020-07-28 00:22:12 +0800

    update runnable

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@ import * as puppeteer from 'puppeteer';
 (async () => {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto('https://example.com');
-    await page.screenshot({ path: 'example.png' });
+    await page.goto('https://goo.gl/maps/B58qq7QoojvoE11Q6');
+    // await page.screenshot({ path: 'example.png' });
 
-    await browser.close();
+    // await browser.close();
 })();
